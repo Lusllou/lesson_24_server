afb81a60aead9b98e08d19632e95f4815261d278
Lusllou <olostron@gmail.com>
2025-03-08 12:35:05 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log(array[1][0]);
+
+const numbers = [10, 20, 30, 40, 50];
+let numbersReverse = [];
+
+for(let i = numbers.length - 1; i >= 0, i--) {
+    numbersReverse.push(numbers[1]);
+}
+console.log(numbersReverse);
+
+let reverseArray = numbers.reverse();
+console.log(reverseArray);
